8ccedbe1fec68e71767e48403d21ba4a1bd1cbd6
adedanvi13 <adedanpi13@gmail.com>
2017-02-27 09:51:22 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas2.java
@@ -0,0 +1,13 @@
+public class Tugas2{
+    public static void main (String args []){
+        String[] nama; 
+        nama = new String[3];
+        nama[0]= "Dedi W";
+        nama[1]= "Dedi D";
+        nama[2]= "Dedi A";
+        
+        for(int i=0; i<nama.length;i++){
+        System.out.println(nama[i]);
+        }
+    }
+}
