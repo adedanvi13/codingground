de258028f687736e9be920be8817b52d1fcd9d6d
adedanvi13 <adedanpi13@gmail.com>
2017-02-27 09:58:37 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas4.java
@@ -0,0 +1,13 @@
+public class Tugas4{
+    public static void main (String args []){
+        int parameter = 1;
+        int hasil = Penjumlahan(parameter);
+        System.out.println("angka penjumlahan " + parameter +" + 2 = " + hasil);
+        }
+        
+        public static int Penjumlahan(int a1) {
+            int angka = a1;
+            
+            return angka + 2;
+    }
+}
